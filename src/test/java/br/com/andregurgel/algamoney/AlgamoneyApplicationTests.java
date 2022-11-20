ee8ad165b6c2c1ad68b1742ab35f8ce346f7ac71
Andre Gurgel <andregurgeldev@gmail.com>
2022-11-20 19:20:03 -0300

    3.2. Conectando ao MySQL

---
+++ src/test/java/br/com/andregurgel/algamoney/AlgamoneyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.andregurgel.algamoney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgamoneyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
