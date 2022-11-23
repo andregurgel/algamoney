d44b905a60cf98be3647b18f0bfe0bb6cc3d6721
Andre Gurgel <andregurgeldev@gmail.com>
2022-11-23 16:52:20 -0300

    3.6. Cadastrando nova categoria com POST

--- src/main/java/br/com/andregurgel/algamoney/resource/CategoriaResource.java
+++ src/main/java/br/com/andregurgel/algamoney/resource/CategoriaResource.java
@@ -3,8 +3,13 @@ package br.com.andregurgel.algamoney.resource;
 import br.com.andregurgel.algamoney.model.Categoria;
 import br.com.andregurgel.algamoney.repository.CategoriaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -19,5 +24,16 @@ public class CategoriaResource {
     @GetMapping
     public List<Categoria> findAll() {
         return categoriaRepository.findAll();
+    }
+
+    @GetMapping("/{categoriaId}")
+    public Categoria findOne(@PathVariable Long categoriaId) {
+        return categoriaRepository.findOne(categoriaId);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Categoria insert(@RequestBody Categoria categoria) {
+        return categoriaRepository.save(categoria);
     }
 }
